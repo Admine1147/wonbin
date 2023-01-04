e482976e82eb77e8798e8fc7f464c123c11b73ff
Admine1147 <117708597+Admine1147@users.noreply.github.com>
2023-01-04 15:03:54 +0900

    master

--- src/layer/controllers/masters.controller.js
+++ src/layer/controllers/masters.controller.js
@@ -1,19 +1,43 @@
-const MasterService = require("../services/masters.services");
+const MasterService = require('../services/masters.services');
 
 class MasterController {
-    masterService = new MasterService();
+  masterService = new MasterService();
 
-    getPage_master_home = async (req, res) => {
-        res.render('master/masterHome');
-    } 
+  getPage_master_home = async (req, res) => {
+    res.render('master/masterHome');
+  };
 
-    getPate_LaundryCare = async (req, res) => {
-        res.render('master/LaundryCare');
+  getPate_LaundryCare = async (req, res) => {
+    res.render('master/LaundryCare');
+  };
+
+  getPage_masterReviews = async (req, res) => {
+    res.render('master/masterReviews');
+  };
+
+  getList_orders = async (req, res) => {
+    const result = await this.masterService.getList_orders();
+    res.json({
+      order_list: result,
+    });
+  };
+
+  accept_order = async (req, res) => {
+    const { order_id } = req.params;
+    const master_id = 1;
+    const exist_order = await this.masterService.getOrder_byMasterId(master_id);
+    if (exist_order) {
+      return res.status(401).send('이미 할 일이 있으니 할 일이나 마저 하십쇼. 인간');
     }
 
-    getPage_masterReviews = async (req, res) => {
-        res.render('master/masterReviews')
-    }
+    const master = await this.masterService.getMaster_byId(master_id);
+    const { storename } = master;
+
+    console.log('con storename : ', storename);
+    const accept_order = await this.masterService.accept_order(order_id, master_id, storename);
+
+    res.json({ accept_order });
+  };
 }
 
 module.exports = MasterController;
--- src/layer/repositories/masters.repositories.js
+++ src/layer/repositories/masters.repositories.js
@@ -1,7 +1,40 @@
-
+const { Op } = require('sequelize');
 
 class MasterRepository {
+  constructor(model) {
+    this.model = model;
+  }
 
+  getList_orders = async () => {
+    const orders_list = await this.model.findAll({ where: { status: 1 } });
+    return orders_list;
+  };
+
+  getOrder_byMasterId = async master_id => {
+    const order = await this.model.findOne({
+      where: { [Op.and]: [{ master_id }, { status: { [Op.gt]: 1 } }, { status: { [Op.lt]: 5 } }] },
+    });
+    return order;
+  };
+
+  getMaster_byId = async master_id => {
+    const master = await this.model.findOne({ where: { master_id } });
+    return master;
+  };
+
+  getOrder_byOrderId = async order_id => {
+    const order = await this.model.findOne({ where: { order_id } });
+    return order;
+  };
+
+  accept_order = async (order_id, master_id, storename, order_status) => {
+    console.log('repo storename : ', storename);
+    const accept_order = await this.model.update(
+      { master_id, storename, status: order_status },
+      { where: { order_id } },
+    );
+    return accept_order;
+  };
 }
 
 module.exports = MasterRepository;
