f53e9439503b8966d97242c9231c5531b43da987
msdou46 <msdou46@gmail.com>
2023-01-04 14:55:17 +0900

    김민수B push용

--- src/routes/user_order/writeComment.routes.js
+++ src/routes/user_order/writeComment.routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const UsersOrderController = require("../../layer/controllers/users.order.controller.js");
+const usersOrderController = new UsersOrderController();
 
+
+router.get('/:order_id', usersOrderController.getUserNameAndMasterStorename);
+router.post('/:order_id', usersOrderController.insertUserReview);
+router.get('/')
 
 
 module.exports = router;
